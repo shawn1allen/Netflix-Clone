5df1b77b613e31debbe0a40a4f1e5f6ae9d53a7d
shawn1allen <51861282+shawn1allen@users.noreply.github.com>
2023-01-19 20:38:39 -0600

    Add files via upload

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,20 +1,20 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropList(id) {
-    console.log(id);
-    document.getElementById(id).classList.toggle("show");
+  console.log(id);
+  document.getElementById(id).classList.toggle("show");
+}
+
+function toggleBottom(el) {
+  const bottomDiv = el.parentNode.querySelector(".bottom");
+  const icon = el.querySelector(".symbol");
+  if(bottomDiv.classList.contains("hide")) {
+    bottomDiv.classList.remove("hide");
+    bottomDiv.classList.add("show");
+    icon.classList.add("rotate");
+  } else {
+    bottomDiv.classList.remove("show");
+    bottomDiv.classList.add("hide");
+    icon.classList.remove("rotate");
   }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+}
