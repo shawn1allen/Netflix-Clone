3f6aa5a4705a262b143c6d63eb6fbd8ea25d1df1
Shawn Allen <shawnallen_tf@yahoo.com>
2023-01-19 21:25:22 -0600

    Added javascript to play each video

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,6 +5,7 @@ function dropList(id) {
   document.getElementById(id).classList.toggle("show");
 }
 
+//Q&A opens the answer when the question is clicked
 function toggleBottom(el) {
   const bottomDiv = el.parentNode.querySelector(".bottom");
   const icon = el.querySelector(".symbol");
@@ -17,4 +18,15 @@ function toggleBottom(el) {
     bottomDiv.classList.add("hide");
     icon.classList.remove("rotate");
   }
+}
+
+window.onload = function() {
+  // Get all video elements on the page
+  var videos = document.getElementsByTagName("video");
+  var videoArray = Array.from(videos);
+  
+  // Play all videos in the array
+  videoArray.forEach(function(video) {
+    video.play();
+  });
 }
