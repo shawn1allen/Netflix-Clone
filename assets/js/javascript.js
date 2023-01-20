c934fbd81bcee60ccf9718b46f1f6e1511b05a0f
Shawn Allen <shawnallen_tf@yahoo.com>
2023-01-20 13:41:06 -0600

    Updated title logo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -32,3 +32,19 @@ window.onload = function() {
     video.loop = true;
   });
 }
+
+//Email validation
+function validateEmail(email, display) {
+  var emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if(email.length < 5) {
+    display.classList.add('error');
+    display.innerHTML = "Email is required!";
+  } else if(!emailRegex.test(email)) {
+    display.classList.add('error');
+    display.innerHTML = "Please enter a valid email address";
+  } else {
+    display.classList.remove('error');
+    display.innerHTML = "";
+  }
+}
